81627205785aaa32a2f2d6d77823107db5b01e4e
John-Karatka <john.karatka@gmail.com>
2021-12-08 21:12:47 -0500

    Update EEPROM_24LC64F.cpp
    added constraint to write function

--- src/EEPROM_24LC64F.cpp
+++ src/EEPROM_24LC64F.cpp
@@ -72,6 +72,8 @@ void EEPROM_24LC64F::busyCheck() {
  * @retval None
  */
 void EEPROM_24LC64F::writeMem(int startAddressHigh, int startAddressLow, int data[], int dataSize) {
+	dataSize = constrain(dataSize, 0, 32);	//the maximum data size that can be transferred at one time is 32 bytes
+
 	Wire.beginTransmission(address);
 	#if ARDUINO >= 100
 		Wire.write(startAddressHigh);
